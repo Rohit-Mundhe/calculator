8049b83e686a63dc98bb2d7a9b9f8c6b18f9a8df
rohit <rohitmundhe14@gmail.com>
2021-08-01 10:55:58 +0530

    started styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+    const [result, setResult] = useState("hello")
     return (
         <div>
-            Hello
+            <div className= "container">
+                <form>
+                    <input type = "text" value = " result "></input>
+                </form>
+                <div className = "keypad">
+                    <button>Clear</button>
+                    <button>C</button>
+                    <button>&divide</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>3</button>
+                    <button>2</button>
+                    <button>1</button>
+                    <button>+</button>
+                    <button>0</button>
+                    <button>-</button>
+                    <button>=</button>
+                </div>
+            </div>
         </div>
     )
 }
